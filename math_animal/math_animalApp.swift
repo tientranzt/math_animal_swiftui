ad84c8b6b843c60dadd67e44898e5e2cf99dd92f
tientran <tientran.home@gmail.com>
2021-01-17 22:18:56 +0700

    Initial Commit

---
+++ math_animal/math_animalApp.swift
@@ -0,0 +1,17 @@
+//
+//  math_animalApp.swift
+//  math_animal
+//
+//  Created by tientran on 17/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct math_animalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
